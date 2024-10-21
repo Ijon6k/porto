2374ad15a6fdb0b212b8303342993d75bf5e27e8
Ijon6k <erikghafari@gmail.com>
2024-10-21 21:51:30 +0700

    desc

--- src/Layouts/LandingPage.jsx
+++ src/Layouts/LandingPage.jsx
@@ -3,9 +3,11 @@ const LandingPage = () => {
     <div className="  max-h-screen h-96 ">
       <div className="  text-center">
         <h1 className="pt-20  font-bold text-3xl">Hello I am Erik Ghafari</h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit,
-          soluta!
+        <p className="w-[50%] mx-auto pt-5">
+          Hi! I am a Front End Developer and second-year college student. I have
+          a passion for multimedia, like photography and editing, which inspire
+          my design work. I enjoy creating clean, user-friendly websites and
+          always look forward to learning more in web development
         </p>
       </div>
     </div>
