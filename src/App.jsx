a917ab64358fd22e210d1eac8f489a857c15aa0e
Ijon6k <erikghafari@gmail.com>
2024-10-21 14:34:24 +0700

    change router

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import {
   BrowserRouter as Router,
   Route,
   Routes,
-  Navigate,
 } from "react-router-dom";
 import Home from "./Pages/Home";
 
@@ -10,9 +9,7 @@ const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Navigate to="home" />} />
-        <Route path="home" element={<Home />} />
-        <Route path="*" element={<Navigate to="home" />} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </Router>
   );
